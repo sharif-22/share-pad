a588f0efbd70df801ec2eb6e135986c7cad46b4f
kajasharif31 <kajasharif366@gmail.com>
2024-03-19 18:21:29 +0530

    fix: Footer at bottom

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mt-auto bg-secondaryBlack">
+      <footer className="container py-3 flex justify-between text-xs">
+        <p>
+          Made with ❤ by
+          <a
+            className="hover:text-blue-500 hover:underline mx-1"
+            href="https://github.com/sharif-22"
+            target="_blank"
+          >
+            Sharif
+          </a>
+          checkout
+          <a
+            className="hover:text-blue-500 hover:underline mx-1"
+            href="https://github.com/sharif-22/share-pad"
+            target="_blank"
+          >
+            Share pad
+          </a>
+          for contribution
+        </p>
+        <p>Copyright {new Date().getFullYear()} - All Rights Reserved</p>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
